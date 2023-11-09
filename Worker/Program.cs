ca1012182e164a4e9c9e419a773d5654a636a55f
David Fowler <davidfowl@gmail.com>
2023-11-08 22:09:04 -0800

    Made worker an app

--- Worker/Program.cs
+++ Worker/Program.cs
@@ -1,11 +1,14 @@
 using WorkerService1;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.AddAzureServiceBus("bus");
 
 builder.AddServiceDefaults();
 builder.Services.AddHostedService<Worker>();
 
-var host = builder.Build();
-host.Run();
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+app.Run();
