6e339431462e30969ee579c5224619ae47d26cd3
David Fowler <davidfowl@gmail.com>
2023-11-12 00:13:05 -0800

    Added comments

--- WebQWorker.AppHost/AzureExtensions.cs
+++ WebQWorker.AppHost/AzureExtensions.cs
@@ -3,6 +3,8 @@
     public static IResourceBuilder<T> UseAzureTracing<T>(this IResourceBuilder<T> builder) 
         where T : IResourceWithEnvironment
     {
+        // Enable Activity Source tracing in the Azure SDK
+        // Enable The Aspire Azure Service bus component's tracing
         return builder.WithEnvironment("AZURE_EXPERIMENTAL_ENABLE_ACTIVITY_SOURCE", "true")
                       .WithEnvironment("Aspire__Azure__Messaging__ServiceBus__Tracing", "true");
     }
